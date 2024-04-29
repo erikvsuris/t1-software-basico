8b69c2756c7c5f18e333c1e2b45dbe07ec5647ad
Erik Suris <87949031+erikvsuris@users.noreply.github.com>
2024-04-29 10:31:25 -0300

    Update hardware_controller.h

--- hardware_controller.h
+++ hardware_controller.h
@@ -5,9 +5,11 @@
 
 void toggleDisplay(unsigned int *reg);
 void setExhibitionMode(unsigned short int *reg, unsigned int newExhibitionMode);
-void setDisplayUpdateSpeed();
-void toggleOperationLED();
-void toggleStatusLEDColor();
-void resetRegisters();
+void setDisplayUpdateSpeed(unsigned short int *reg);
+void toggleOperationLED(unsigned short int *reg);
+void setStatusColorRed(unsigned short int *reg);
+void setStatusColorGreen(unsigned short int *reg);
+void setStatusColorBlue(unsigned short int *reg);
+void resetRegisters(unsigned short int *reg);
 
 #endif
